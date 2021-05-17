ef0923a33d1d470fa55b2f7dd3a57edbd5cd31fd
lizelive <github@lize.live>
2021-05-16 19:32:44 -0700

    fix open font

---
+++ BlazorWebAssemblyDB/Client/RazorTagHelper.cs
@@ -0,0 +1,86 @@
+﻿
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyDB.Client
+{
+    public class MagicUnicorn
+    {
+        public static void Run()
+        {
+            //Create an index and define a text analyzer
+
+            // Ensures index backward compatibility
+            const LuceneVersion AppLuceneVersion = LuceneVersion.LUCENE_48;
+
+            // Construct a machine-independent path for the index
+            var basePath = Environment.GetFolderPath(
+                Environment.SpecialFolder.CommonApplicationData);
+
+            using var dir = new RAMDirectory();
+
+            // Create an analyzer to process the text
+            var analyzer = new StandardAnalyzer(AppLuceneVersion);
+
+            // Create an index writer
+            var indexConfig = new IndexWriterConfig(AppLuceneVersion, analyzer);
+            using var writer = new IndexWriter(dir, indexConfig);
+
+
+            //Add to the index
+
+            var source = new
+            {
+                Name = "Kermit the Frog",
+                FavoritePhrase = "The quick brown fox jumps over the lazy dog"
+            };
+            var doc = new Document
+            {
+                // StringField indexes but doesn't tokenize
+                new StringField("name",
+                    source.Name,
+                    Field.Store.YES),
+                new TextField("favoritePhrase",
+                    source.FavoritePhrase,
+                    Field.Store.YES)
+            };
+
+            writer.AddDocument(doc);
+            writer.Flush(triggerMerge: false, applyAllDeletes: false);
+
+            // Search with a phrase
+            var phrase = new MultiPhraseQuery
+            {
+                new Term("favoritePhrase", "brown"),
+                new Term("favoritePhrase", "fox")
+            };
+
+            // Re-use the writer to get real-time updates
+            using var reader = writer.GetReader(applyAllDeletes: true);
+            var searcher = new IndexSearcher(reader);
+            var hits = searcher.Search(phrase, 20 /* top 20 */).ScoreDocs;
+
+            // Display the output in a table
+            Console.WriteLine($"{"Score",10}" +
+                $" {"Name",-15}" +
+                $" {"Favorite Phrase",-40}");
+            foreach (var hit in hits)
+            {
+                var foundDoc = searcher.Doc(hit.Doc);
+                Console.WriteLine($"{hit.Score:f8}" +
+                    $" {foundDoc.Get("name"),-15}" +
+                    $" {foundDoc.Get("favoritePhrase"),-40}");
+            }
+
+        }
+    }
+}
